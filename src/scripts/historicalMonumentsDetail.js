a08dc3f1f0fa832fd657ccbc98559659269836fe
vitaliy abrosimov <vitalikabrosimov00@gmail.com>
2026-03-31 18:13:02 +0300

    added qualification-assessment-center page

--- src/scripts/historicalMonumentsDetail.js
+++ src/scripts/historicalMonumentsDetail.js
@@ -2,23 +2,7 @@ import {Swiper} from "swiper";
 import {Pagination} from "swiper/modules";
 
 document.addEventListener('DOMContentLoaded', () => {
-    const menuList = document.querySelector('.historical-monuments-detail__menu-list')
-    const menuLinks = document.querySelectorAll('.historical-monuments-detail__menu-link')
-
-    menuLinks.forEach(link => {
-        const menuLink = document.createElement('a')
-        menuLink.href = `#${link.id}`
-        menuLink.classList.add('section__menu-link')
-        menuLink.textContent = link.textContent
-
-        const menuItem = document.createElement('li')
-        menuItem.classList.add('section__menu-links-item')
-        menuItem.appendChild(menuLink)
-
-        menuList.appendChild(menuItem)
-    })
-
-    const swiperBlocks = document.querySelectorAll('.historical-monuments-detail .swiper')
+    const swiperBlocks = document.querySelectorAll('.historical-monuments-detail__img-block .swiper')
 
     swiperBlocks.forEach((block, index) => {
         const swiper = new Swiper(block, {
