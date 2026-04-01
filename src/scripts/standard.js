c2db80dc6c0a86eed6b3b61a33a9734f40942269
vitaliy abrosimov <vitalikabrosimov00@gmail.com>
2026-04-01 15:54:45 +0300

    added standard-3 page; corrected tables in standards pages; corrected margin-top in footer

--- src/scripts/standard.js
+++ src/scripts/standard.js
@@ -1,32 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const paginationBullets = document.querySelectorAll('.standard__pagination span')
-    const table = document.querySelector('.standard__table-wrapper')
+    const tables = document.querySelectorAll('.standard__table-wrapper')
 
-    if (!paginationBullets?.length || ! table) return
-
-    const rect = table.getBoundingClientRect()
+    if (!tables?.length) return
 
     const classes = {
         isActive: 'is-active'
     }
 
-    const clearAllIsActive = () => {
-        paginationBullets.forEach(bullet => {
-            bullet.classList.remove(classes.isActive)
+    tables.forEach(table => {
+        const pagination = table?.parentNode?.querySelector('.standard__pagination')
+
+        if (!pagination) return
+
+        pagination.innerHTML = ''
+
+        const trs = table.querySelectorAll('thead th')
+        trs.forEach((tr) => {
+            const bullet = document.createElement('span')
+            pagination.appendChild(bullet)
         })
-    }
 
-    table.addEventListener('scroll', () => {
-        const progress = table.scrollLeft / (table.scrollWidth - rect.width)
+        const paginationBullets = pagination.querySelectorAll('span')
 
-        clearAllIsActive()
+        if (!paginationBullets?.length) return
 
-        if (progress <= 0.33) {
-            paginationBullets[0].classList.add(classes.isActive)
-        } else if (progress >= 0.33 && progress <= 0.66) {
-            paginationBullets[1].classList.add(classes.isActive)
-        } else {
-            paginationBullets[2].classList.add(classes.isActive)
-        }
+        paginationBullets[0].classList.add(classes.isActive)
+
+        const rect = table.getBoundingClientRect()
+
+        table.addEventListener('scroll', () => {
+            const progress = table.scrollLeft / (table.scrollWidth - rect.width)
+            if (progress <= 0) return
+
+            let activeIndex = Math.round(progress * (paginationBullets.length - 1))
+
+            activeIndex = Math.max(0, Math.min(activeIndex, paginationBullets.length - 1))
+
+            paginationBullets.forEach((bullet, index) => {
+                bullet.classList.toggle(classes.isActive, index === activeIndex)
+            })
+        })
     })
 })
