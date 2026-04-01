b2d167cd9c3c5bb06daf4b58dfacf039e5bbd3e5
vitaliy abrosimov <vitalikabrosimov00@gmail.com>
2026-04-01 11:01:18 +0300

    added standard page

---
+++ src/scripts/standard.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const paginationBullets = document.querySelectorAll('.standard__pagination span')
+    const table = document.querySelector('.standard__table')
+
+    if (!paginationBullets?.length || ! table) return
+
+    const rect = table.getBoundingClientRect()
+
+    const classes = {
+        isActive: 'is-active'
+    }
+
+    const clearAllIsActive = () => {
+        paginationBullets.forEach(bullet => {
+            bullet.classList.remove(classes.isActive)
+        })
+    }
+
+    table.addEventListener('scroll', () => {
+        const progress = table.scrollLeft / (table.scrollWidth - rect.width)
+
+        clearAllIsActive()
+
+        if (progress <= 0.33) {
+            paginationBullets[0].classList.add(classes.isActive)
+        } else if (progress >= 0.33 && progress <= 0.66) {
+            paginationBullets[1].classList.add(classes.isActive)
+        } else {
+            paginationBullets[2].classList.add(classes.isActive)
+        }
+    })
+})
